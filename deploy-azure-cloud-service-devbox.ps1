d76035b3cfb40dcc4c1833c2de5caef38a876f35
Guang Yang <allenmacyoung@gmail.com>
2013-06-06 18:49:37 -0700

    working on cscfg generation

--- deploy-azure-cloud-service-devbox.ps1
+++ deploy-azure-cloud-service-devbox.ps1
@@ -1,7 +1,6 @@
 ﻿Param(
     [Parameter(Mandatory = $true)]
-    [String]$ProjectFile,
-    [String]$Name
+    [String]$ProjectFile
 )
 
 $VerbosePreference = "Continue"
@@ -16,10 +15,46 @@ $scriptPath = Split-Path -parent $PSCommandPath
 $publishDir = "{0}\" -f $scriptPath
 
 # Generate the cscfg and cspkg files
-& "$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $ProjectFile /t:Publish /p:TargetProfile=Cloud /p:PublishDir=$publishDir
+# & "$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $ProjectFile /t:Publish /p:TargetProfile=Cloud /p:PublishDir=$publishDir
+
+[Xml]$envXml = Get-Content ("{0}\cloud-service-environment.xml" -f $scriptPath)
+[Xml]$cscfgXml = Get-Content ("{0}\ServiceConfiguration.Cloud.cscfg" -f (Get-Item $ProjectFile).DirectoryName)
+
+Foreach ($role in $cscfgXml.ServiceConfiguration.Role)
+{
+
+    Foreach ($setting in $role.ConfigurationSettings)
+    {
+        If ($setting.FirstChild.name -eq "Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString")
+        {
+            $setting.FirstChild.value = $envXml.environment.storage.connectionString.ToString()
+            Break
+        }
+    }
+
+    # Add the connection string for appdb
+    $setting1 = $cscfgXml.CreateElement("Setting", "http://schemas.microsoft.com/ServiceHosting/2008/10/ServiceConfiguration")
+    $setting1.SetAttribute("name", "appdb")
+    $setting1.SetAttribute("value", $envXml.environment.sqlAzure.appdb.connectionString)
+    $temp = $role.ConfigurationSettings.AppendChild($setting1)
+
+    # Add the connection string for DefaultConnection
+    $setting2 = $cscfgXml.CreateElement("Setting", "http://schemas.microsoft.com/ServiceHosting/2008/10/ServiceConfiguration")
+    $setting2.SetAttribute("name", "DefaultConnection")
+    $setting2.SetAttribute("value", $envXml.environment.sqlAzure.DefaultConnection.connectionString)
+    $temp = $role.ConfigurationSettings.AppendChild($setting2)
+}
+
+$cscfgXml.InnerXml
+
+<#
 
 $cscfg = "{0}\ServiceConfiguration.Cloud.cscfg" -f $scriptPath
 $cspkg = "{0}\{1}.cspkg" -f $scriptPath, (Get-Item $ProjectFile).BaseName
+
+# Read from cloud-service-environment.xml to get the environment name
+[Xml]$envXml = Get-Content ("{0}\cloud-service-environment.xml" -f $scriptPath)
+$cloudServiceName = $envXml.environment.name
 
 # If there is no existing deployment on the cloud service, create a new deployment
 # Otherwise, upgrade the deployment using simultaneous mode
@@ -27,15 +62,15 @@ $cspkg = "{0}\{1}.cspkg" -f $scriptPath, (Get-Item $ProjectFile).BaseName
 $deployment = $null
 Try
 {
-    $deployment = Get-AzureDeployment -ServiceName $Name
+    $deployment = Get-AzureDeployment -ServiceName $cloudServiceName
 }
 Catch
 {
-    New-AzureDeployment -ServiceName $Name -Slot Production -Configuration $cscfg -Package $cspkg
+    New-AzureDeployment -ServiceName $cloudServiceName -Slot Production -Configuration $cscfg -Package $cspkg
 }
 If ($deployment)
 {
-    Set-AzureDeployment -ServiceName $Name -Slot Production -Configuration $cscfg -Package $cspkg -Mode Simultaneous -Upgrade
+    Set-AzureDeployment -ServiceName $cloudServiceName -Slot Production -Configuration $cscfg -Package $cspkg -Mode Simultaneous -Upgrade
 }
 
 
@@ -43,3 +78,4 @@ If ($deployment)
 $finishTime = Get-Date
 # Output the time consumed in seconds
 Write-Output ("Total time used (seconds): {0}" -f ($finishTime - $startTime).TotalSeconds)
+#>
